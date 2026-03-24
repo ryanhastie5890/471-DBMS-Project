0cbe40e502dc43f8cb22c30e7a5892a23ce59e6c
Ryan Hastie <231561729+ryanhastie5890@users.noreply.github.com>
2026-03-23 22:55:29 -0400

    Create DBMS.java

---
+++ DBMS.java
@@ -0,0 +1,26 @@
+/*
+ * This is the main DBMS file for the 471 Project by Ishaq Ahmed, Justin Haynes, and Ryan Hastie
+ * 
+ * This file will hold the logic for the dbms
+ */
+
+import java.util.Scanner;
+
+public class DBMS {
+	
+	public static void main(String [] args) {
+		Scanner scn = new Scanner(System.in);
+		
+		
+		System.out.println("Welcome to DBMS");
+		
+		Boolean done = false;
+		while(!done) {
+			System.out.println("Enter Command:");
+			String command = scn.nextLine();
+			GrammarParser parser = new GrammarParser(command);
+			parser.beginParse();
+		}
+	}
+
+}
